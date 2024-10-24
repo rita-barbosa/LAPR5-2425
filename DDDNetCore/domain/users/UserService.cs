3deb7059467827ee2f8465591e252f7d6ba849b2
Ana Guterres <1221933@isep.ipp.pt>
2024-10-25 00:39:56 +0100

    DG38-92 | feat: adds methods to reset user password to UserService.

--- DDDNetCore/domain/users/UserService.cs
+++ DDDNetCore/domain/users/UserService.cs
@@ -22,7 +22,6 @@ namespace DDDNetCore.Domain.Users
         private readonly RoleManager<Role> _roleManager;
         private readonly StaffService _staffService;
         private readonly EmailService _emailService;
-        private readonly LogService _logService;
         private readonly TokenService _tokenService;
         private readonly IConfiguration _configuration;
         public UserService(UserManager<User> userManager, RoleManager<Role> roleManager,
@@ -129,6 +128,16 @@ namespace DDDNetCore.Domain.Users
             return true;
         }
 
+        public async Task<bool> UserExistsByEmail(string email)
+        {
+            User user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
         public async Task<User> FindByIdAsync(string userId)
         {
             return await _userManager.FindByIdAsync(userId);
@@ -195,6 +204,14 @@ namespace DDDNetCore.Domain.Users
             await SendEmail(user.Email, "Account Activation", body);
         }
 
+        private async Task SendPasswordEmail(User user, string token)
+        {
+            string confirmationLink = await ConfigureUrlPasswordConfirmation(token, user);
+            var body = "<p>Hello,</p><p>This email was sent to allow you to change your password in your HealthCare Clinic System account. </p><p><a href='" + confirmationLink + "'>Click in the link to change your password.</a></p><p>Thank you for choosing us,<br>HealthCare Clinic</p></body></html>";
+
+            await SendEmail(user.Email, "Password Reset", body);
+        }
+
         private async Task<string> ConfigureUrlConfirmation(string token, User user)
         {
             string? role = (await _userManager.GetRolesAsync(user)).FirstOrDefault();
@@ -207,6 +224,22 @@ namespace DDDNetCore.Domain.Users
             ? $"{baseUrl}/Activate-PatientAccount?userId={user.Id}&token={encodedToken}"
             : $"{baseUrl}/Activate-StaffAccount?userId={user.Id}&token={encodedToken}";
         }
+
+        private async Task<string> ConfigureUrlPasswordConfirmation(string token, User user)
+        {
+            string? role = (await _userManager.GetRolesAsync(user)).FirstOrDefault();
+
+            if (role == null) throw new NullReferenceException("Can't obtain the user role.");
+
+            var encodedToken = Uri.EscapeDataString(token);
+            var baseUrl = _configuration["App:BaseUrl"];
+            if(role.Equals("Admin") || role.Equals("Technician") || role.Equals("Doctor") || role.Equals("Nurse"))
+                return $"{baseUrl}/Update-UserPassword?email={user.Email}&token={encodedToken}";
+
+
+            throw new BusinessRuleValidationException("Invalid user role. Only Admin, Doctor, Technician, or Nurse are allowed.");
+        }
+
         private async Task SendEmail(string receiver, string subject, string body)
         {
             if (receiver == null) throw new NullReferenceException("The recipient's email cannot be null.");
@@ -220,13 +253,48 @@ namespace DDDNetCore.Domain.Users
             await _emailService.SendConfirmationEmail(emailDto);
         }
 
+        public async Task ResetPassword(string email)
+        {
+            User user = await _userManager.FindByEmailAsync(email) ?? throw new BusinessRuleValidationException("Can't find the currently logged in user.");
+            user.changeStatus(false);
+            await _userManager.UpdateAsync(user);
+            await SendPasswordEmail(user,  await _userManager.GenerateEmailConfirmationTokenAsync(user));
+        }
+
+        public async Task<bool> UpdatePassword(string email, string token, string newPassword)
+        {
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(token))
+                {
+                    throw new Exception("User Email and Token are required.");
+                }
+
+            if (!await UserExistsByEmail(email))
+            {
+                throw new Exception("User not found.");
+            }
+
+            string userId = (await FindByEmailAsync(email)).Id;
+            var result = await ConfirmEmailToken(userId, token);
+            if (result)
+            {
+                User user = await FindByIdAsync(userId);
+                var resetPasswordToken = await _tokenService.GeneratePasswordResetTokenAsync(user);
+                await ResetPasswordAsync(user, resetPasswordToken, newPassword);
+                user.changeStatus(true);
+                await UpdateAsync(user);
+                return true;
+            }
+            return false;
+        }
+
+
         public async Task<string> DeleteAsync(string userId)
         {
             var user = await _userManager.FindByIdAsync(userId);
 
             if (user == null)
                 return null;
-            
+
             await _userManager.DeleteAsync(user);
             await _logService.CreateDeletionLog(user.Id.ToString(), user.GetType().Name, "Deletion of user account.");
 
