2dee484198106c0e551b1fedd4cde1bef52866a1
Rita Beatriz Ferreira Barbosa <1220841@isep.ipp.pt>
2024-10-27 00:49:12 +0100

    DG38-127 | test: adds integration tests for list of operation requests; updates existent tests.

--- MDBackofficeTests/integrationtests/operationtype/GetFilteredOperationTypesIntegrationTests.cs
+++ MDBackofficeTests/integrationtests/operationtype/GetFilteredOperationTypesIntegrationTests.cs
@@ -1,9 +1,11 @@
-﻿using DDDNetCore.Domain.Logs;
+﻿using DDDNetCore.Controllers;
+using DDDNetCore.Domain.Logs;
 using DDDNetCore.Domain.OperationTypes.ValueObjects.Phase;
 using DDDNetCore.Domain.OperationTypes.ValueObjects.RequiredStaff;
 using DDDNetCore.Domain.OperationTypes;
 using DDDNetCore.Domain.OperationTypesRecords;
 using DDDNetCore.Domain.Shared;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -11,33 +13,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
-using DDDNetCore.Controllers;
-using Microsoft.AspNetCore.Mvc;
 
-namespace MDBackofficeTests.controllertests
+namespace MDBackofficeTests.integrationtests.operationtype
 {
-    public class OperationTypeControllerTests
+    public class GetFilteredOperationTypesIntegrationTests
     {
         private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
         private readonly Mock<LogService> _logServiceMock = new(new Mock<IUnitOfWork>().Object, new Mock<ILogRepository>().Object);
         private readonly Mock<IOperationTypeRepository> _repoMock = new Mock<IOperationTypeRepository>();
         private readonly Mock<OperationTypeRecordService> _opRecordService;
-        private readonly Mock<OperationTypeService> _service;
-        private readonly OperationTypesController _controller;
+        private readonly OperationTypeService _service;
+      
 
-        public OperationTypeControllerTests()
+        public GetFilteredOperationTypesIntegrationTests()
         {
             _opRecordService = new Mock<OperationTypeRecordService>(_unitOfWorkMock.Object, _logServiceMock.Object, new Mock<IOperationTypeRecordRepository>().Object);
 
-            _service = new Mock<OperationTypeService>(_unitOfWorkMock.Object, _repoMock.Object, _logServiceMock.Object, _opRecordService.Object);
+            _service = new OperationTypeService(_unitOfWorkMock.Object, _repoMock.Object, _logServiceMock.Object, _opRecordService.Object);
 
-            _controller = new OperationTypesController(_service.Object);
+            
         }
 
         [Fact]
-        public async Task GetFilteredOperationTypes_ReturnsOkOperationTypeDtos()
+        public async Task GetFilteredOperationTypes_ReturnsOkOperationTypeDtos_IntegrationControllerService()
         {
             // Arrange
+            var _controller = new OperationTypesController(_service);
+
             var queryParameters = new OperationTypeQueryParametersDto
             {
                 queryFilters = new List<OperationTypeListingFilterParametersDto>
@@ -110,5 +112,70 @@ namespace MDBackofficeTests.controllertests
         }
 
 
+        [Fact]
+        public async Task FilterOperationTypes_ReturnsOperationTypeDtos_IntegrationServiceDomain()
+        {
+            // Arrange
+            var queryParameters = new OperationTypeQueryParametersDto
+            {
+                queryFilters = new List<OperationTypeListingFilterParametersDto>
+                {
+                    new OperationTypeListingFilterParametersDto
+                    {
+                        Name = "test type 1",
+                        Specialization = "ortho",
+                        Status = "Active"
+                    }
+                }
+            };
+            var phasesDto = new List<PhaseDto>
+            {
+                new PhaseDto {
+                            Description = "descrip",
+                            Duration = 25
+                            },
+                new PhaseDto {
+                            Description = "descrip2",
+                            Duration = 50
+                            },
+                new PhaseDto {
+                            Description = "descrip3",
+                            Duration = 25
+                            }
+            };
+            var reqStaffDto = new List<RequiredStaffDto>
+            {
+                new RequiredStaffDto{
+                    StaffQuantity = 1,
+                    Function = "doctor",
+                    Specialization = "ortho"
+                }
+            };
+            var operationTypes = new List<OperationType>
+            {
+                new OperationType("test type 1", 100, true, reqStaffDto,phasesDto),
+            };
+            var expectedDtos = new List<OperationTypeDto>
+            {
+                new OperationTypeDto{Name ="test type 1",EstimatedDuration = 100, Status = true,RequiredStaff =reqStaffDto,Phases = phasesDto }
+            };
+
+            _repoMock.Setup(repo => repo.FilterOperationTypes(queryParameters))
+                    .ReturnsAsync(operationTypes);
+
+            // Act
+            var result = await _service.FilterOperationTypes(queryParameters);
+
+            // Assert
+            Assert.Equal(expectedDtos.Count, result.Count);
+            for (int i = 0; i < expectedDtos.Count; i++)
+            {
+                Assert.Equal(expectedDtos[i].Name, result[i].Name);
+                Assert.Equal(expectedDtos[i].EstimatedDuration, result[i].EstimatedDuration);
+                Assert.Equal(expectedDtos[i].Status, result[i].Status);
+
+            }
+            _repoMock.Verify(repo => repo.FilterOperationTypes(queryParameters), Times.Once);
+        }
     }
 }
