11e2b98bfd93c86b665bf878107eb16887b392c9
Rita Beatriz Ferreira Barbosa <1220841@isep.ipp.pt>
2024-12-18 11:14:31 +0000

    DG38-155 | feat: adds error handling for token and role validation in the MDPatientManagement module.

--- MDPatientManagement/src/api/routes/roleRoute.ts
+++ MDPatientManagement/src/api/routes/roleRoute.ts
@@ -2,18 +2,31 @@ import { Router } from 'express';
 import { celebrate, Joi } from 'celebrate';
 
 import { Container } from 'typedi';
-import IRoleController from '../../controllers/IControllers/IRoleController'; 
+import IRoleController from '../../controllers/IControllers/IRoleController';
 
 import config from "../../../config";
+import middlewares from '../middlewares';
+import isAuth from '../middlewares/isAuth';
+import isAuthz from '../middlewares/isAuthz';
 
 const route = Router();
 
 export default (app: Router) => {
   app.use('/roles', route);
 
+  app.use(function (err, req, res, next) {  /// YOU GUYS NEED TO ADD THIS
+    if (err.name === "UnauthorizedError") {
+      res.status(401).send("Invalid or missing token.");
+    } else {
+      next(err);
+    }
+  });
+
   const ctrl = Container.get(config.controllers.role.name) as IRoleController;
 
   route.post('',
+    middlewares.isAuth,                /// YOU GUYS NEED TO ADD THIS
+    middlewares.isAuthz(["Admin"]),    /// YOU GUYS NEED TO ADD THIS
     celebrate({
       body: Joi.object({
         name: Joi.string().required()
