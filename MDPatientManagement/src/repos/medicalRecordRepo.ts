66c966cb0d9132fdc1c868273b14483c1f9359ba
Ana Guterres <1221933@isep.ipp.pt>
2024-12-20 20:35:55 +0000

    DG38-538 | feat: corrects repo implementation.

---
+++ MDPatientManagement/src/repos/medicalRecordRepo.ts
@@ -0,0 +1,72 @@
+import { Inject, Service } from "typedi";
+import IMedicalRecordRepo from "../services/IRepos/IMedicalRecordRepo";
+import { IMedicalRecordPersistence } from "../dataschema/IMedicalRecordPersistence";
+import { Document, FilterQuery, Model } from "mongoose";
+import { MedicalRecordMap } from "../mappers/MedicalRecordMap";
+import { MedicalRecord } from "../domain/medicalRecord";
+import { MedicalRecordId } from "../domain/medicalRecordId";
+
+@Service()
+export default class MedicalRecordRepo implements IMedicalRecordRepo {
+    private models: any;
+
+    constructor(
+        @Inject('medicalRecordSchema') private medicalRecordSchema: Model<IMedicalRecordPersistence & Document>,
+        @Inject('logger') private logger
+    ) { }
+    
+    public async save(medicalRecord: MedicalRecord): Promise<MedicalRecord> {
+
+        const query = { domainId: medicalRecord.id.toString() };
+
+        const medicalRecordDocument = await this.medicalRecordSchema.findOne(query);
+
+        try {
+            if (medicalRecordDocument != null) {
+                throw new Error(`Medical Record with ID ${medicalRecord.medicalRecordId} already exists.`);
+            }
+
+            const rawMedicalRecord: any = MedicalRecordMap.toPersistence(medicalRecord);
+
+            const medicalRecordCreated = await this.medicalRecordSchema.create(rawMedicalRecord);
+
+            return MedicalRecordMap.toDomain(medicalRecordCreated);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    public async exists(medicalRecord: MedicalRecord): Promise<boolean> {
+        
+        const idX = medicalRecord.id instanceof MedicalRecordId ? (<MedicalRecordId>medicalRecord.id).toValue() : medicalRecord.id;
+    
+        const query = { domainId: idX}; 
+        const medicalRecordDocument = await this.medicalRecordSchema.findOne( query as FilterQuery<IMedicalRecordPersistence & Document>);
+    
+        return !!medicalRecordDocument === true;
+      }
+
+    public async findByDomainId(medicalRecordId: MedicalRecordId | string): Promise<MedicalRecord> {
+        const query = { domainId: medicalRecordId };
+        const medicalRecordRecord = await this.medicalRecordSchema.findOne(query as FilterQuery<IMedicalRecordPersistence & Document>);
+
+        if (medicalRecordRecord != null) {
+            return MedicalRecordMap.toDomain(medicalRecordRecord);
+        }
+        else
+            return null;
+    }
+
+    public async findAll(): Promise<MedicalRecord[]> {
+        try {
+          const medicalRecordRecords = await this.medicalRecordSchema.find({});
+          const medicalRecordList = await Promise.all(
+            medicalRecordRecords.map(async (record) => await MedicalRecordMap.toDomain(record))
+          );
+    
+          return medicalRecordList;
+        } catch (error) {
+          this.logger.error("Error fetching all medical record:", error);
+        }
+      }
+}
--- MDPatientManagement/src/services/IRepos/IMedicalRecordRepo.ts
+++ MDPatientManagement/src/services/IRepos/IMedicalRecordRepo.ts
@@ -2,7 +2,7 @@ import { Repo } from "../../core/infra/Repo";
 import { MedicalRecord } from "../../domain/medicalRecord";
 import { MedicalRecordId } from "../../domain/medicalRecordId";
 
-export default interface IMedicalREcordRepo extends Repo<MedicalRecord> {
+export default interface IMedicalRecordRepo extends Repo<MedicalRecord> {
     save(medicalRecord: MedicalRecord): Promise<MedicalRecord>;
     findByDomainId (medicalRecordId: MedicalRecordId | string): Promise<MedicalRecord>;
     findAll() : Promise<MedicalRecord[]>
