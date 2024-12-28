03d4d63d3a11467e9fd80b4556b3548fd7c1b8a1
Rita Beatriz Ferreira Barbosa <1220841@isep.ipp.pt>
2024-12-28 22:02:32 +0000

    DG38-155 | chore: deletes unnecessary import.

--- MDPatientManagement/src/api/middlewares/index.ts
+++ MDPatientManagement/src/api/middlewares/index.ts
@@ -1,9 +1,7 @@
-import attachCurrentUser from './attachCurrentUser';
 import isAuth from './isAuth';
 import isAuthz from './isAuthz';
 
 export default {
-  attachCurrentUser,
   isAuth,
   isAuthz
 };
