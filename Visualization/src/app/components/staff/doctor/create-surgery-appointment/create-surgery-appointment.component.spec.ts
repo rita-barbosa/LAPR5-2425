7be6e5ef171dd6754bac27aab8dda62fbef5702c
Matilde Xavier Varela <1220683@isep.ipp.pt>
2024-12-29 07:24:08 +0000

    DG38-548 | test: frontend unit tests

--- Visualization/src/app/components/staff/doctor/create-surgery-appointment/create-surgery-appointment.component.spec.ts
+++ Visualization/src/app/components/staff/doctor/create-surgery-appointment/create-surgery-appointment.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of, throwError } from 'rxjs';
+import { of } from 'rxjs';
 import { CreateSurgeryAppointmentComponent } from './create-surgery-appointment.component';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
