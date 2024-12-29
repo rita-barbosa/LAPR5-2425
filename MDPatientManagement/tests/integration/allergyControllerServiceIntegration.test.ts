8d4314b251b498c86004ba8cf6a8098a9c54b6de
Ana Guterres <1221933@isep.ipp.pt>
2024-12-29 19:03:55 +0000

    DG38-473 | test: adds integration tests for the allergy creation.

--- MDPatientManagement/tests/integration/allergyControllerServiceIntegration.test.ts
+++ MDPatientManagement/tests/integration/allergyControllerServiceIntegration.test.ts
@@ -10,6 +10,7 @@ import IAllergyService from "../../src/services/IServices/IAllergyService";
 import { Result } from "../../src/core/logic/Result";
 import IAllergyRepo from "../../src/services/IRepos/IAllergyRepo";
 import { AllergyMap } from "../../src/mappers/AllergyMap";
+import { Allergy } from "../../src/domain/allergy";
 
 describe("Allergy Controller+Service Integration Tests", function () {
     const sandbox = sinon.createSandbox();
@@ -42,6 +43,48 @@ describe("Allergy Controller+Service Integration Tests", function () {
         sandbox.restore();
         sinon.restore();
     });
+
+    it("should create an allergy successfully with allergyRepo and allergy stub", async () => {
+        // Arrange
+        const allergyDTO: IAllergyDTO = {
+            code: 'BZ04',
+            designation: 'Peanut Allergy',
+            description: 'An allergic reaction to peanuts.',
+        };
+    
+        let body = allergyDTO;
+        let req: Partial<Request> = {};
+        req.body = body;
+        let res: Partial<Response> = {
+            json: sinon.spy()
+        };
+        const next: Partial<NextFunction> = sinon.spy();
+    
+        const createStub = sinon.stub(Allergy, "create").resolves({
+            isFailure: false,
+            getValue: () => allergyDTO,
+        });
+    
+        const mapStub = sinon.stub(AllergyMap, "toDTO").returns(allergyDTO);
+    
+        const allergyRepoInstance = Container.get("AllergyRepo") as IAllergyRepo;
+        const saveStub = sinon.stub(allergyRepoInstance, "save").resolves(allergyDTO);
+    
+        const allergyServiceInstance = Container.get("AllergyService") as IAllergyService;
+        const ctrl = new AllergyController(allergyServiceInstance);
+    
+        // Act
+        await ctrl.createAllergy(<Request>req, <Response>res, <NextFunction>next);
+    
+        // Assert
+        sinon.assert.calledOnce(createStub);
+        sinon.assert.calledOnce(saveStub);
+        sinon.assert.calledOnce(mapStub);
+        sinon.assert.calledOnce(res.json);
+        sinon.assert.calledWith(res.json, sinon.match(allergyDTO));
+    });
+
+
     it("should return allergies when found with allergyRepo and allergy stub", async function () {
         const filter: IAllergyQueryFilterParametersDTO = {
             queryfilters: [{ code: 'BZ02.2' }],
