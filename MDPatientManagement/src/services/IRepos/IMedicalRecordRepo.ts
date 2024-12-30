b08a422ded0a40d8228f0ed78dc7c77530bfd6e3
Ana Guterres <1221933@isep.ipp.pt>
2024-12-30 17:23:35 +0000

    DG38-495 | test: corrects test error.

--- MDPatientManagement/src/services/IRepos/IMedicalRecordRepo.ts
+++ MDPatientManagement/src/services/IRepos/IMedicalRecordRepo.ts
@@ -2,6 +2,7 @@ import { Repo } from "../../core/infra/Repo";
 import { MedicalRecord } from "../../domain/medicalRecord";
 import { MedicalRecordId } from "../../domain/medicalRecordId";
 import { IMedicalRecordQueryFilterParameters } from "../../dto/IMedicalRecordQueryFilterParameters";
+import { IMedicalRecordQueryFilterParametersById } from "../../dto/IMedicalRecordQueryFiltersIds";
 
 export default interface IMedicalRecordRepo extends Repo<MedicalRecord> {
     findAllByParameters(filters: IMedicalRecordQueryFilterParametersById): Promise<MedicalRecord[]>;
