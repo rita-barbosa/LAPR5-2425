13a7e0be08290b9c49ac182491b49c394456c418
Ana Guterres <1221933@isep.ipp.pt>
2025-01-02 11:44:07 +0000

    DG38-638 | test: adds unit tests for frontend to export medical record.

--- Visualization/src/app/components/patient/export-medical-record/export-medical-record.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { ExportMedicalRecordComponent } from './export-medical-record.component'
-
-describe('ExportMedicalRecordComponent', () => {
-  it('should mount', () => {
-    cy.mount(ExportMedicalRecordComponent)
-  })
-})
